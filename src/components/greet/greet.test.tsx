ea7354999813c5bb22d8cc9706f160577ce49767
Adetomi <ribigbetomi@gmail.com>
2023-07-30 17:05:03 +0100

    test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,15 +6,19 @@ import { Greet } from "./greet";
 //   const textElement = screen.getByText(/hello/i);
 //   expect(textElement).toBeInTheDocument();
 // });
-
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Adetomi" />);
-  const textElement = screen.getByText(`Hello Adetomi`);
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("renders with a name", () => {
+    //test.skip() or test.only()
+    render(<Greet name="Adetomi" />);
+    const textElement = screen.getByText(`Hello Adetomi`);
+    expect(textElement).toBeInTheDocument();
+  });
 });
