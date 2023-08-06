f864d55695b7bc230099e29ef94ad39fe3181f2b
Adetomi <ribigbetomi@gmail.com>
2023-08-06 21:38:47 +0100

    mockFunctions

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import "./App.css";
 import { Application } from "./components/application/application";
 import { Skills } from "./components/skills/skills";
 import { Counter } from "./components/counter/counter";
+import { AppProviders } from "./providers/appProviders";
+import { MuiMode } from "./components/mui/muiMode";
+import { CounterTwo } from "./components/counter-two/counterTwo";
 
 function App() {
   return (
-    <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS"]} />
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        {/* <Application />
+        <Skills skills={["HTML", "CSS"]} />
+        <Counter /> */}
+        <MuiMode />
+        <CounterTwo count={1} />
+      </div>
+    </AppProviders>
   );
 }
 
